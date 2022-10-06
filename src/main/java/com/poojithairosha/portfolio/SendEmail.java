8fe081fd3be15a08a57c64d9aaf4b8235513511e
bmpsolutions <softwaresolutionsbmp@gmail.com>
2022-10-06 17:46:37 +0530

    Validated user inputs

--- src/main/java/com/poojithairosha/portfolio/SendEmail.java
+++ src/main/java/com/poojithairosha/portfolio/SendEmail.java
@@ -1,5 +1,7 @@
 package com.poojithairosha.portfolio;
 
+import org.jetbrains.annotations.NotNull;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,7 +28,7 @@ public class SendEmail extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(@NotNull HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // reads form fields
         String name = request.getParameter("name");
         String recipient = request.getParameter("email"); //
@@ -34,20 +36,28 @@ public class SendEmail extends HttpServlet {
         String content = request.getParameter("message");
 
         String resultMessage = "";
-        String message = "<h2>Message From Portfolio</h2>" +
-                "<br/>" +
-                "Name: " + name + "<br>" +
-                "Email: " + recipient + "<br>" +
-                "Message: " + content + "<br>";
 
-        try {
-            EmailUtility.sendEmail(host, port, user, pass, user, subject, message, recipient);
-            resultMessage = "success";
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            resultMessage = "There were an error: " + ex.getMessage();
-        } finally {
-            response.getWriter().write(resultMessage);
+        if (name.isEmpty()) {
+            resultMessage = "Please enter your name";
+        } else if (recipient.isEmpty()) {
+            resultMessage = "Please enter your email address";
+        } else if (subject.isEmpty()) {
+            resultMessage = "Please enter the subject";
+        } else if (content.isEmpty()) {
+            resultMessage = "Please enter the message";
+        } else {
+            String message = "<h2>Message From Portfolio</h2>" + "<br/>" + "Name: " + name + "<br>" + "Email: " + recipient + "<br>" + "Message: " + content + "<br>";
+
+            try {
+                EmailUtility.sendEmail(host, port, user, pass, user, subject, message, recipient);
+                resultMessage = "success";
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                resultMessage = "There were an error: " + ex.getMessage();
+            }
         }
+
+        response.getWriter().write(resultMessage);
+
     }
 }
